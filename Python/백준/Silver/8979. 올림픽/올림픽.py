0a290e712337d923edc754be59f4a7199bf771ca
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-08-02 01:25:11 +0900

    [Silver V] Title: 올림픽, Time: 48 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Silver/8979. 올림픽/올림픽.py
+++ Python/백준/Silver/8979. 올림픽/올림픽.py
@@ -5,19 +5,20 @@ medal = [[0, 0, 0, 0] for _ in range(N)]
 
 for i in range(N):
     medal[i][0], medal[i][1], medal[i][2], medal[i][3] = map(int, sys.stdin.readline().strip().split())
-    
-medal.sort(key=lambda x : (x[1], x[2], x[3]), reverse=True)
+
+medal.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
 
 team_rank = [1] + [0 for _ in range(N-1)]
-rank = 2
+rank = 1
 
 for i in range(1, N):
     if medal[i][1] == medal[i-1][1] and medal[i][2] == medal[i-1][2] and medal[i][3] == medal[i-1][3]:
         team_rank[i] = team_rank[i-1]
     else:
+        rank = i + 1
         team_rank[i] = rank
-    
-    rank += 1
 
-print(team_rank[K-1])
-        
+for i in range(N):
+    if medal[i][0] == K:
+        print(team_rank[i])
+        break
