ce069201617fd3b316a5ed439ec9c6517e812f70
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-08-27 01:56:49 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/42748. K번째수/K번째수.py
+++ Python3/프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,12 @@
+# i, j, k
+# i-1에서 j까지 슬라이싱
+# 슬라이싱된 리스트의 k-1 인덱스값 return
+
 def solution(array, commands):
     answer = []
     for i in range(len(commands)):
-        arr = array[commands[i][0]-1:commands[i][1]]
-        arr.sort()
-        answer.append(arr[commands[i][2]-1])
+        temp = array[commands[i][0]-1 : commands[i][1]]
+        temp.sort()
+        answer.append(temp[commands[i][2]-1])
+        
     return answer
