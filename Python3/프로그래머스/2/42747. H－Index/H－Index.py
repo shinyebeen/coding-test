90b19bd2684d29c64f5461f5ff68e0adcd0db8c0
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-09-03 03:45:29 +0900

    [level 2] Title: H-Index, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,3 +1,16 @@
 def solution(citations):
-    answer = 0
-    return answer
+    citations.sort(reverse=True)
+  
+    for i in range(len(citations)):
+        if citations[i] <= i:
+            return i
+    return len(citations)
+
+#     for h in range(citations[0], -1, -1):
+#         cnt = 0
+#         for c in citations:
+#             if h <= c:
+#                 cnt += 1
+        
+#         if cnt >= h:
+#             return h
--- Python3/프로그래머스/2/42747. H－Index/README.md
+++ Python3/프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 401.10 ms
+메모리: 10.3 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 03:34:16
+2024년 09월 03일 03:45:29
 
 ### 문제 설명
 
