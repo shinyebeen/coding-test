044b9a190d14626ac949ecba4507532b6d7122ca
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-10-24 05:43:20 +0900

    [Gold IV] Title: 도서관, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Gold/1461. 도서관/도서관.py
@@ -0,0 +1,41 @@
+n, m = map(int, input().split())
+position = list(map(int, input().split()))
+
+pos, neg = [0], [0]
+
+for i in position:
+    if i > 0:
+        pos.append(i)
+    else:
+        neg.append(-i)
+
+pos.sort()
+neg.sort()
+
+max_num = max(pos[-1], neg[-1])
+answer = 0
+
+
+while pos:
+    cnt = 0
+
+    answer += pos[-1] * 2
+    cnt += 1
+    pos.pop()
+
+    while pos and cnt < m:
+        cnt += 1
+        pos.pop()
+
+while neg:
+    cnt = 0
+
+    answer += neg[-1] * 2
+    cnt += 1
+    neg.pop()
+
+    while neg and cnt < m:
+        cnt += 1
+        neg.pop()
+
+print(answer - max_num)
