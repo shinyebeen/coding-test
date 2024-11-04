73a71e4b4979e9a1ea3988d63bc766a34dfe23c9
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-11-05 02:35:29 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ Python/백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,35 +1,31 @@
 def row(li):
-    answer = 0
+    answer = [0, 0]
     for i in range(n):
-        cnt = 0
+        cnt_r, cnt_c = 0, 0
         for j in range(n):
+            # 가로
             if li[i][j] == '.':
-                cnt += 1
+                cnt_r += 1
             else:
-                if cnt >= 2:
-                    answer += 1
-                cnt = 0
-        if cnt >= 2:
-            answer += 1
-    return answer
-
-def col(li):
-    answer = 0
-    for i in range(n):
-        cnt = 0
-        for j in range(n):
+                if cnt_r >= 2:
+                    answer[0] += 1
+                cnt_r = 0
+            # 세로
             if li[j][i] == '.':
-                cnt += 1
+                cnt_c += 1
             else:
-                if cnt >= 2:
-                    answer += 1
-                cnt = 0
-        if cnt >= 2:
-            answer += 1
+                if cnt_c >= 2:
+                    answer[1] += 1
+                cnt_c = 0
+                
+        if cnt_r >= 2:
+            answer[0] += 1
+        if cnt_c >= 2:
+            answer[1] += 1
 
     return answer
 
 n = int(input())
 arr = [input() for _ in range(n)]
-print(row(arr), end=' ')
-print(col(arr))
+
+print(*row(arr))
