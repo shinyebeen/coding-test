091c436b23a2193a1dbe8a60149cafcc56207356
Shin Yebeen <160686479+shinyebeen@users.noreply.github.com>
2024-12-01 23:00:27 +0900

    [Gold V] Title: LCS, Time: 508 ms, Memory: 55708 KB -BaekjoonHub

---
+++ Python/백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,11 @@
+s1 = list(input())
+s2 = list(input())
+dp = [[0]*(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1] == s2[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+print(max(map(max, dp)))
